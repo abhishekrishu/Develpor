093a93ff6871648e18dd023f81c9814b38f6a6f2
abhishek singh <riahu.abhishek40@gmail.com>
2016-11-17 12:11:44 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/develpor/inheritance.java
@@ -0,0 +1,141 @@
+package com.example.android.develpor;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by android on 11/17/2016.
+ */
+
+public class inheritance extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inheritance);
+    }
+}
+class Animal {
+
+    public Animal() {
+
+        System.out.println("A new animal has been created!");
+
+    }
+
+
+
+    public void sleep() {
+
+        System.out.println("An animal sleeps...");
+
+    }
+
+
+
+    public void eat() {
+
+        System.out.println("An animal eats...");
+
+    }
+
+}
+class Bird extends Animal {
+
+    public Bird() {
+
+        super();
+
+        System.out.println("A new bird has been created!");
+
+    }
+
+
+
+
+
+    public void sleep() {
+
+        System.out.println("A bird sleeps...");
+
+    }
+
+
+
+
+
+    public void eat() {
+
+        System.out.println("A bird eats...");
+
+    }
+
+}
+
+class Cow extends Animal {
+
+    public Cow() {
+
+        super();
+
+        System.out.println("A new cow has been created!");
+
+    }
+
+
+
+
+
+    public void sleep() {
+
+        System.out.println("A cow sleeps...");
+
+    }
+
+
+
+
+
+    public void eat() {
+
+        System.out.println("A cow eats...");
+
+    }
+
+}
+class MainClass {
+
+    public static void main(String[] args) {
+
+        Animal animal = new Animal();
+
+        Bird bird = new Bird();
+
+        Cow cow = new Cow();
+
+
+
+        System.out.println();
+
+
+
+        animal.sleep();
+
+        animal.eat();
+
+
+
+        bird.sleep();
+
+        bird.eat();
+
+
+
+        cow.sleep();
+
+        cow.eat();
+
+    }
+
+}
+
+
