302f9d15acf933a2a61b5c7a4f3c79b257c363a2
abhishek singh <riahu.abhishek40@gmail.com>
2016-11-17 13:10:23 +0530

    rdvf w

--- app/src/main/java/com/example/android/develpor/inheritance.java
+++ app/src/main/java/com/example/android/develpor/inheritance.java
@@ -102,40 +102,5 @@ class Cow extends Animal {
     }
 
 }
-class MainClass {
-
-    public static void main(String[] args) {
-
-        Animal animal = new Animal();
-
-        Bird bird = new Bird();
-
-        Cow cow = new Cow();
-
-
-
-        System.out.println();
-
-
-
-        animal.sleep();
-
-        animal.eat();
-
-
-
-        bird.sleep();
-
-        bird.eat();
-
-
-
-        cow.sleep();
-
-        cow.eat();
-
-    }
-
-}
 
 
